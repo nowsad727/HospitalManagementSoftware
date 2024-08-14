94bbf469b1f08b4bed14b713217c459c8ed64bef
nowsad727 <nowsadahamedkazi@gmail.com>
2024-08-14 17:31:40 +0600

    Initial commit

---
+++ src/main/java/com/hms/controller/HospitalManagementSoftware.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hms.controller;
+
+import com.hms.view.Home;
+
+/**
+ *
+ * @author Hello World
+ */
+public class HospitalManagementSoftware {
+
+    public static void main(String[] args) {
+      new Home().setVisible(true);
+    }
+}
---
+++ src/main/java/com/hms/model/PathologicalTest.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hms.model;
+
+/**
+ *
+ * @author Hello World
+ */
+public class PathologicalTest {
+    String title;
+    double cost;
+    boolean isAvailable;
+
+    
+    PathologicalTest( String title,double cost,boolean isAvailable){
+        this.title=title;
+        this.cost=cost;
+        this.isAvailable=isAvailable;
+                
+    }
+    
+    
+    String show() { 
+        String output = "Test name: " + this.title + "<br>"
+                        +"Cost: " + this.cost + "<br>"
+                + "Availability: " + this.isAvailable;
+        return output;
+    }
+    
+    void search (String title) {
+            if (this.title.equals(title)) {
+        System.out.println(show());
+        
+    }        
+            else 
+                System.out.println ("Not found");
+}
+}
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+
